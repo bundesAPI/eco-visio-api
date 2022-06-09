8c3c0aea154ab3d38408e068b2c82cebe593c45e
Poschi <development@poschi.net>
2022-06-09 20:47:32 +0200

    Initial commit WIP

---
+++ test.sh
@@ -0,0 +1,7 @@
+#curl 'https://www.eco-visio.net/api/aladdin/1.0.0/pbl/publicwebpageplus/4586'
+
+curl 'https://www.eco-visio.net/api/aladdin/1.0.0/pbl/publicwebpageplus/data/100158705?idOrganisme=4586&idPdc=100158705&fin=26%2F05%2F2022&debut=01%2F01%2F2022&interval=4&flowIds=101158705%3B102158705%3B103158705%3B104158705'
+
+#curl 'https://www.eco-visio.net/api/aladdin/1.0.0/pbl/publicwebpageplus/4586?withNull=true'
+ # -H 'Accept: text/plain, */*; q=0.01' 
+#   -H 'Origin: https://data.eco-counter.com'
